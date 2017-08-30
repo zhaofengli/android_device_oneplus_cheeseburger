b44681fd015495a1168b85a8a5e980d03ad40c93
Hamster Tian <haotia@gmail.com>
2017-08-31 00:23:31 +0800

    Revert "op5: enable split A2DP to implement A2DP encoding offload"
    * CAF requires blobs.
    
    This reverts commit e5fbb8a1ddeb120b3a5b267898494499d7afec20.

--- device.mk
+++ device.mk
@@ -155,7 +155,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
-    libbthost_if
 
 # Camera
 PRODUCT_PACKAGES += \
